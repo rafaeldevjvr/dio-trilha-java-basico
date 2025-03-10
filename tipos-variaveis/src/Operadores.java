522d9b5ea071a5123cf56c6d013ad45c0e72ad02
rafaeldevjvr <rafaeldevprintln@gmail.com>
2025-03-10 12:44:16 -0300

    Criando smartTv e suas funções

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,27 +1,13 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        String concatenacao = "?";
+        int a, b;
+        a = 5;
+        b = 6;
 
-        concatenacao = 1+1+1+"1";
+        String resultado = a==b ?"verdadeiro" : "falso";
 
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+1+1+1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+(1+1+1);
-
-        System.out.println(concatenacao);
+        System.out.println(resultado);
 
     }
     
